8f2af36370a10159073768f2042761cdcc1ae780
Alenajess <93519022+Alenajess@users.noreply.github.com>
2021-11-09 04:33:34 +0300

    Add files via upload

---
+++ l4/task1.c
@@ -0,0 +1,6 @@
+#include <math.h>
+
+double f(const double *x, const double *y, double *result)
+{
+	*result = ((*x - 1)*sqrt(*x) - (*y - 1)*sqrt(*y)) / (sqrt(pow(*x, 3)**y) + *x * *y + pow(*x, 2) - *x);
+}
